6261a2d128fdbb521d73c48c7f79118820869d8e
Soumya kesarwani <soumyakesarwani70@gmail.com>
2025-10-28 22:44:37 +0530

    changes

--- wp-content/themes/dentia-child/functions.php
+++ wp-content/themes/dentia-child/functions.php
@@ -29,3 +29,39 @@ add_action( 'wp_enqueue_scripts', 'dentia_child_enqueue_styles' );
 /**
  * Add your custom functions below this line
  */
+
+/**
+ * Fix breadcrumb labels for custom post types
+ */
+function dentia_child_fix_breadcrumb_labels( $entries, $post ) {
+    $post_type = get_post_type( $post );
+
+    // Check if we have entries
+    if ( empty( $entries ) ) {
+        return $entries;
+    }
+
+    // For Service post type
+    if ( $post_type === 'service' && isset( $entries[0] ) ) {
+        $entries[0]['label'] = 'Services';
+        $entries[0]['url'] = home_url( '/all-services/' );
+    }
+
+    // For Speciality post type
+    if ( $post_type === 'speciality' && isset( $entries[0] ) ) {
+        $entries[0]['label'] = 'Specialities';
+        $entries[0]['url'] = home_url( '/specialities/' );
+    }
+
+    return $entries;
+}
+add_filter( 'dentia_breadcrumb_single', 'dentia_child_fix_breadcrumb_labels', 10, 2 );
+
+/**
+ * Temporary debug function to check what's happening
+ */
+add_action( 'wp_footer', function() {
+    if ( is_singular( 'speciality' ) ) {
+        echo '<!-- Filter is registered: ' . ( has_filter( 'dentia_breadcrumb_single' ) ? 'YES' : 'NO' ) . ' -->';
+    }
+} );
